26bfd9935b8945ad4e18c0d851a4f2a2d9cf4bb8
RubenSmn <saaman.ruben@gmail.com>
2022-06-11 10:25:28 +0200

    feat(binary-tree): add depth first values

---
+++ binary_tree/depth-first-values.js
@@ -0,0 +1,16 @@
+// problem from
+// https://www.structy.net/problems/depth-first-values
+
+// iterative approach
+const depthFirstValues = (root) => {
+  let visited = [];
+  let stack = [root];
+  while (stack.length >= 0) {
+    const curr = stack.pop();
+    if (curr === null) continue;
+    visited.push(curr.val);
+    stack.push(curr.right);
+    stack.push(curr.left);
+  }
+  return visited;
+};
