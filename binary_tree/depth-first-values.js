934e56e348ad4c69c2c7afe99089529282f695c5
RubenSmn <saaman.ruben@gmail.com>
2022-06-12 10:30:11 +0200

    feat(binary-tree): add recursive

--- binary_tree/depth-first-values.js
+++ binary_tree/depth-first-values.js
@@ -14,3 +14,11 @@ const depthFirstValues = (root) => {
   }
   return visited;
 };
+
+// recursive approach
+const depthFirstValues = (root) => {
+  if (root === null) return [];
+  const leftValues = depthFirstValues(root.left);
+  const rightValues = depthFirstValues(root.right);
+  return [ root.val, ...leftValues, ...rightValues ];
+};
