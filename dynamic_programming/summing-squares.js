bf5b4247ba651957bb9dca55af4c73afaea2b410
RubenSmn <saaman.ruben@gmail.com>
2022-07-02 09:41:30 +0200

    feat(dp): add summing squares

---
+++ dynamic_programming/summing-squares.js
@@ -0,0 +1,16 @@
+// problem from
+// https://www.structy.net/problems/summing-squares
+
+const summingSquares = (n, memo={}) => {
+  if (n in memo) return memo[n];
+  if (n === 0) return n;
+
+  let minSquares = Infinity;
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    const square = i * i;
+    const numSquares = 1 + summingSquares(n - square, memo);
+    minSquares = Math.min(numSquares, minSquares);
+  }
+  memo[n] = minSquares;
+  return minSquares;
+};
